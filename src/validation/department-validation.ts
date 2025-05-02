72427cea062d0c246abb3cb858d560d43a3946f3
andumpangestu <andumpangestu12345@gmail.com>
2025-05-02 13:32:48 +0700

    feat: add number validation in departement validation

--- src/validation/department-validation.ts
+++ src/validation/department-validation.ts
@@ -1,15 +1,17 @@
-import { z, ZodType } from "zod";
+import { number, z, ZodType } from "zod";
 
 export class DepartmentValidation {
 
     static readonly CREATE: ZodType = z.object({
         code: z.string().min(1).max(100),
-        name: z.string().min(1).max(100)
+        name: z.string().min(1).max(100),
+        number: z.string().min(1).max(100),
     });
 
     static readonly UPDATE: ZodType = z.object({
         code: z.string().min(1).max(100),
-        name: z.string().min(1).max(100)
+        name: z.string().min(1).max(100),
+        number: z.string().min(1).max(100),
     });
 
 
