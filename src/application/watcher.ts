784c51cefbbdbf35ec7daf8dad052ac67c2180a3
andumpangestu <andumpangestu12345@gmail.com>
2025-09-29 13:38:09 +0700

    feat: minor update

--- src/application/watcher.ts
+++ src/application/watcher.ts
@@ -10,96 +10,155 @@ import { JsService } from "../service/js-service";
 import { SHARED_FOLDER_PATH } from "./config";
 import { prismaClient } from "./database";
 import fileQueue from "./file-queue";
+import { PurchaseRequestOtherDepartmentService } from "../service/purchase-request-other-department-service";
 
-
-
-const purchaseRequestFolderPath = path.join(SHARED_FOLDER_PATH, "purchase-request");
+const purchaseRequestFolderPath = path.join(
+  SHARED_FOLDER_PATH,
+  "purchase-request"
+);
+const purchaseRequestOtherDepartmentFolderPath = path.join(
+  SHARED_FOLDER_PATH,
+  "purchase-request-other-department"
+);
 const purchaseOrderFolderPath = path.join(SHARED_FOLDER_PATH, "purchase-order");
-const receivingReportFolderPath = path.join(SHARED_FOLDER_PATH, "receiving-report");
+const receivingReportFolderPath = path.join(
+  SHARED_FOLDER_PATH,
+  "receiving-report"
+);
 const jsFolderPath = path.join(SHARED_FOLDER_PATH, "js-ending-quantity");
 const kanbanMasterFolderPath = path.join(SHARED_FOLDER_PATH, "kanban-master");
 
-
-
-
 const startWatcher = () => {
-    logger.info("🟢 Starting watcher..." + SHARED_FOLDER_PATH);
-    const watcher = chokidar.watch([purchaseRequestFolderPath, purchaseOrderFolderPath, receivingReportFolderPath, jsFolderPath, kanbanMasterFolderPath], {
-        persistent: true,
-        ignoreInitial: true,
-        usePolling: true,
-        ignored: /(^|[\/\\])~\$/,
-        awaitWriteFinish: {
-            stabilityThreshold: 2000,
-            pollInterval: 100,
-        },
-        ignorePermissionErrors: true,
-    });
+  logger.info("🟢 Starting watcher..." + SHARED_FOLDER_PATH);
+  const watcher = chokidar.watch(
+    [
+      purchaseRequestFolderPath,
+      purchaseOrderFolderPath,
+      receivingReportFolderPath,
+      jsFolderPath,
+      kanbanMasterFolderPath,
+      purchaseRequestOtherDepartmentFolderPath,
+    ],
+    {
+      persistent: true,
+      ignoreInitial: true,
+      usePolling: true,
+      ignored: /(^|[\/\\])~\$/,
+      awaitWriteFinish: {
+        stabilityThreshold: 2000,
+        pollInterval: 100,
+      },
+      ignorePermissionErrors: true,
+    }
+  );
 
+  (watcher as any)
+    .on("add", (filePath: string) => {
+      fileQueue.add(async () => {
+        const normalizedPath = path.normalize(filePath);
+        const ext = path.extname(normalizedPath).toLowerCase();
 
-    (watcher as any).on("add", (filePath: string) => {
-        fileQueue.add(async () => {
-            const normalizedPath = path.normalize(filePath);
-            const ext = path.extname(normalizedPath).toLowerCase();
+        const allowedExts = [".xlsx", ".xls", ".xlsb", ".xlsm", ".csv", ".ods"];
+        if (!allowedExts.includes(ext)) return;
 
-            const allowedExts = [".xlsx", ".xls", ".xlsb", ".xlsm", ".csv", ".ods"];
-            if (!allowedExts.includes(ext)) return;
+        const fileName = path.basename(normalizedPath, ext);
+        logger.info(`🟢 File added: ${normalizedPath}`);
 
-            const fileName = path.basename(normalizedPath, ext);
-            logger.info(`🟢 File added: ${normalizedPath}`);
+        const fileTypes = [
+          {
+            type: "PO",
+            folderPath: purchaseOrderFolderPath,
+            regex: /^PO_\d{2}\d{2}\d{4}_(?:[1-9]|[1-9]\d{1,2}|1000)$/,
+            service: PurchaseOrderService,
+          },
+          {
+            type: "PR",
+            folderPath: purchaseRequestFolderPath,
+            regex: /^PR_\d{2}\d{2}\d{4}_(?:[1-9]|[1-9]\d{1,2}|1000)$/,
+            service: PurchaseRequestService,
+          },
+          {
+            type: "PR_OD",
+            folderPath: purchaseRequestOtherDepartmentFolderPath,
+            regex: /^PR_OD_\d{2}\d{2}\d{4}_(?:[1-9]|[1-9]\d{1,2}|1000)$/,
+            service: PurchaseRequestOtherDepartmentService,
+          },
+          {
+            type: "RR",
+            folderPath: receivingReportFolderPath,
+            regex: /^RR_\d{2}\d{2}\d{4}_(?:[1-9]|[1-9]\d{1,2}|1000)$/,
+            service: ReceivingReportService,
+          },
+          {
+            type: "JS",
+            folderPath: jsFolderPath,
+            regex: /^JS_\d{2}\d{2}\d{4}_(?:[1-9]|[1-9]\d{1,2}|1000)$/,
+            service: JsService,
+          },
+          {
+            type: "KM",
+            folderPath: kanbanMasterFolderPath,
+            regex: /^KM_\d{2}\d{2}\d{4}_(?:[1-9]|[1-9]\d{1,2}|1000)$/,
+            service: KanbanService,
+          },
+        ];
 
-            const fileTypes = [
-                { type: "PO", folderPath: purchaseOrderFolderPath, regex: /^PO_\d{2}\d{2}\d{4}_(?:[1-9]|[1-9]\d{1,2}|1000)$/, service: PurchaseOrderService },
-                { type: "PR", folderPath: purchaseRequestFolderPath, regex: /^PR_\d{2}\d{2}\d{4}_(?:[1-9]|[1-9]\d{1,2}|1000)$/, service: PurchaseRequestService },
-                { type: "RR", folderPath: receivingReportFolderPath, regex: /^RR_\d{2}\d{2}\d{4}_(?:[1-9]|[1-9]\d{1,2}|1000)$/, service: ReceivingReportService },
-                { type: "JS", folderPath: jsFolderPath, regex: /^JS_\d{2}\d{2}\d{4}_(?:[1-9]|[1-9]\d{1,2}|1000)$/, service: JsService },
-                { type: "KM", folderPath: kanbanMasterFolderPath, regex: /^KM_\d{2}\d{2}\d{4}_(?:[1-9]|[1-9]\d{1,2}|1000)$/, service: KanbanService },
-            ];
-
-            const matched = fileTypes.find(({ folderPath }) => normalizedPath.includes(folderPath));
-            if (!matched) {
-                logger.warn(`⚠️ File ${fileName} not in PO/PR/RR/JS/KM folder`);
-                return;
-            }
-
-            const { type, regex, service } = matched;
-            if (!regex.test(fileName)) {
-                logger.warn(`⚠️ File name ${type} does not match: ${fileName}`);
-                return;
-            }
-
-            try {
-                const alreadyExist = await ProcessedFileService.isAlreadyExist(fileName, type);
-                if (alreadyExist) {
-                    logger.warn(`⚠️ File ${fileName} with type ${type} already processed`);
-                    return;
-                }
-
-                await prismaClient.$transaction(async (tx) => {
-                    await service.create(normalizedPath);
-                    await tx.processedFile.create({
-                        data: { file_name: fileName, type: type }
-                    });
-                }, { timeout: 60000 });
-
-                logger.info(`✅ File ${type} successfully processed: ${fileName}`);
-            } catch (error: any) {
-                logger.error(`❌ Failed to process file ${normalizedPath}: ${error.message}`);
-            }
+        const matched = fileTypes.find(({ folderPath }) => {
+          const dirPath = path.dirname(normalizedPath);
+          return dirPath === folderPath;
         });
+
+        if (!matched) {
+          logger.warn(`⚠️ File ${fileName} not in PO/PR/PR_OD/RR/JS/KM folder`);
+          return;
+        }
+
+        const { type, regex, service } = matched;
+        if (!regex.test(fileName)) {
+          logger.warn(`⚠️ File name ${type} does not match: ${fileName}`);
+          return;
+        }
+
+        try {
+          const alreadyExist = await ProcessedFileService.isAlreadyExist(
+            fileName,
+            type
+          );
+          if (alreadyExist) {
+            logger.warn(
+              `⚠️ File ${fileName} with type ${type} already processed`
+            );
+            return;
+          }
+
+          await prismaClient.$transaction(
+            async (tx) => {
+              await service.create(normalizedPath);
+              await tx.processedFile.create({
+                data: { file_name: fileName, type: type },
+              });
+            },
+            { timeout: 60000 }
+          );
+
+          logger.info(`✅ File ${type} successfully processed: ${fileName}`);
+        } catch (error: any) {
+          logger.error(
+            `❌ Failed to process file ${normalizedPath}: ${error.message}`
+          );
+        }
+      });
     })
 
-
-
-        .on("change", async (filePath: string) => {
-            logger.info(`🟡 File changed: ${filePath}`);
-        })
-        .on("unlink", async (filePath: string) => {
-            logger.info(`🔴 File deleted: ${filePath}`);
-        })
-        .on("error", async (error: Error) => {
-            logger.error(`❌ Error: ${error}`);
-        });
+    .on("change", async (filePath: string) => {
+      logger.info(`🟡 File changed: ${filePath}`);
+    })
+    .on("unlink", async (filePath: string) => {
+      logger.info(`🔴 File deleted: ${filePath}`);
+    })
+    .on("error", async (error: Error) => {
+      logger.error(`❌ Error: ${error}`);
+    });
 };
 
 export default startWatcher;
---
+++ src/service/purchase-request-other-department-service.ts
@@ -0,0 +1,278 @@
+import xlsx from "xlsx";
+import { Validation } from "../validation/validation";
+import { PurchaseRequestValidation } from "../validation/purchase-request-validation";
+import { PurchaseRequestDetailValidation } from "../validation/purchase-request-detail-validation";
+import { prismaClient } from "../application/database";
+import {
+  CreatePurchaseRequestRequest,
+  PurchaseRequestResponse,
+  PurchaseRequestRawEntry,
+  SearchPurchaseRequestRequest,
+  toPurchaseRequestResponse,
+} from "../model/purchase-request-model";
+import {
+  PurchaseRequestDetailRawEntry,
+  CreatePurchaseRequestDetailRequest,
+} from "../model/purchase-request-detail-model";
+import { logger } from "../application/logging";
+import { convertDate } from "../helper/convert-date-helper";
+import { ResponseError } from "../error/response-error";
+import { Pageable } from "../model/page";
+import { log } from "console";
+
+export class PurchaseRequestOtherDepartmentService {
+  static async create(filePath: string) {
+    const workbook: xlsx.WorkBook = xlsx.readFile(filePath);
+
+    // Ambil sheet (pastikan nama sheet sesuai)
+    const sheet: xlsx.WorkSheet = workbook.Sheets["Sheet1"];
+
+    // Konversi sheet ke JSON
+    const data: any[][] = xlsx.utils.sheet_to_json(sheet, {
+      header: 1,
+    });
+
+    const headers: string[] = data[4] as string[];
+
+    const importantHeaders: string[] = [
+      "Date",
+      "PR No.",
+      "Department",
+      "Budget No.",
+      "Fixed Asset No",
+      "Type",
+      "Transportation",
+      "Kind of Request",
+      "Requested",
+      "Gen. Manager",
+      "Supervisor",
+    ];
+
+    const missingHeaders: string[] = importantHeaders.filter(
+      (h) => !headers.includes(h)
+    );
+    if (missingHeaders.length > 0) {
+      logger.error(
+        `Missing important headers in file ${filePath}: ${missingHeaders.join(
+          ", "
+        )}`
+      );
+      throw new Error(
+        `Missing important headers in file ${filePath}: ${missingHeaders.join(
+          ", "
+        )}`
+      );
+    }
+
+    const isValidProductCode = (code: string): boolean => {
+      const prefix = code.split("-")[0];
+      return prefix === "EA";
+    };
+
+    const purchaseRequests: PurchaseRequestRawEntry[] = [];
+    const purchaseRequestDetails: PurchaseRequestDetailRawEntry[] = [];
+
+    for (let i = 5; i < data.length - 1; i++) {
+      if (data[i].length === 0) continue;
+
+      const obj: PurchaseRequestRawEntry = {};
+      const objDetail: PurchaseRequestDetailRawEntry = {};
+      for (let j = 0; j < headers.length; j++) {
+        obj[headers[j]] = data[i][j];
+        objDetail[headers[j]] = data[i][j];
+      }
+
+      const isPurposeOnly: boolean =
+        data[i].filter(Boolean).length === 1 && Boolean(objDetail["Purpose"]);
+
+      if (isPurposeOnly && purchaseRequestDetails.length > 0) {
+        purchaseRequestDetails[purchaseRequestDetails.length - 1][
+          "Purpose"
+        ] += ` ${objDetail["Purpose"]}`;
+        continue;
+      }
+
+      const isContinuationRow: boolean = importantHeaders.every(
+        (header) => !obj[header]
+      );
+
+      if (isContinuationRow && purchaseRequests.length > 0) {
+        objDetail["PR No."] =
+          purchaseRequests[purchaseRequests.length - 1]["PR No."];
+      } else {
+        objDetail["PR No."] = obj["PR No."];
+        purchaseRequests.push(obj);
+      }
+
+      purchaseRequestDetails.push(objDetail);
+    }
+
+    const parseNumber = (val: string | undefined): number | null =>
+      val && val !== "-" ? Number(val) : null;
+
+    const parseInteger = (val: string | undefined): number | null => {
+      if (!val || val === "-") return null; // undefined, string kosong, atau tanda minus saja
+      const n = parseInt(val, 10); // buang desimal
+      return Number.isNaN(n) ? null : n; // NaN -> null
+    };
+
+    const parseString = (val: string | undefined): string | null =>
+      val && val !== "-" ? val.toString() : null;
+
+    const parseDate = (val: string | undefined): Date | null =>
+      val && val !== "-" ? convertDate(val) : null;
+
+    const formattedResult: CreatePurchaseRequestRequest[] =
+      purchaseRequests.map((entry: PurchaseRequestRawEntry) => {
+        return {
+          date: parseDate(entry["Date"]),
+          pr_number: parseString(entry["PR No."]),
+          department: parseString(entry.Department),
+          budget_number: parseString(entry["Budget No."]),
+          fixed_asset_number: parseString(entry["Fixed Asset No"]),
+          type: parseString(entry.Type),
+          transportation: parseString(entry.Transportation),
+          kind_of_request: parseString(entry["Kind of Request"]),
+          requested: parseString(entry.Requested),
+          gen_manager: parseString(entry["Gen. Manager"]),
+          supervisor: parseString(entry.Supervisor),
+        };
+      });
+
+    const detailFormattedResult: CreatePurchaseRequestDetailRequest[] =
+      purchaseRequestDetails.map((entry: PurchaseRequestRawEntry) => {
+        return {
+          pr_number: parseString(entry["PR No."]),
+          acc: parseString(entry.Acc),
+          kanban_code: parseString(entry["Item Code"]),
+          item_name: parseString(entry["Item Name"]),
+          description_of_goods: parseString(entry["Description of Goods"]),
+          specification: parseString(entry.Specification),
+          part: parseString(entry.Part),
+          quantity: parseInteger(entry.Quantity),
+          unit: parseString(entry.Unit),
+          est_unit_price: parseNumber(entry["Est. Unit Price"]),
+          est_amount: parseNumber(entry["Est. Amount"]),
+          currency: parseString(entry.Currency),
+          req_delivery: parseDate(entry["Req. Delivery"]),
+          supplier: parseString(entry.Supplier),
+          remark: parseString(entry.Remark),
+          purpose: parseString(entry.Purpose),
+        };
+      });
+
+    try {
+      // Validasi data
+      let createRequest = Validation.validate(
+        PurchaseRequestValidation.CREATE,
+        formattedResult
+      );
+
+      let createRequestDetail = Validation.validate(
+        PurchaseRequestDetailValidation.CREATE,
+        detailFormattedResult
+      ).filter(
+        (item) =>
+          item.kanban_code === null || isValidProductCode(item.kanban_code)
+      );
+
+      const kanbanCodes = createRequestDetail
+        .map((item) => item.kanban_code)
+        .filter((code): code is string => Boolean(code));
+
+      // Cari kanban code yang sudah ada di DB
+      const existingKanban = await prismaClient.kanban.findMany({
+        where: { code: { in: kanbanCodes } },
+        select: { code: true },
+      });
+
+      const existingKanbanCodes = new Set(existingKanban.map((k) => k.code));
+
+      // Deteksi dan buat kanban yang tidak ada
+      const missingKanban = createRequestDetail.filter(
+        (item) => item.kanban_code && !existingKanbanCodes.has(item.kanban_code)
+      );
+
+      if (missingKanban.length > 0) {
+        const newKanbans = missingKanban.map((item) => ({
+          code: item.kanban_code!,
+          description: item.description_of_goods!,
+          specification: item.specification!,
+          uom: item.unit!,
+          price: item.est_unit_price!,
+          currency: item.currency!,
+        }));
+
+        await prismaClient.$transaction(async (tx) => {
+          await tx.kanban.createMany({
+            data: newKanbans,
+            skipDuplicates: true,
+          });
+        });
+
+        logger.info(
+          `Created kanban codes from ${filePath}: ${missingKanban
+            .map((i) => i.kanban_code)
+            .join(", ")}`
+        );
+      }
+
+      if (createRequestDetail.length === 0) {
+        const msg = `All kanban codes in file ${filePath} do not exist in database`;
+        logger.error(msg);
+        throw new Error(msg);
+      }
+
+      // Cek apakah PR number sudah ada
+      const prNumbers = createRequest
+        .map((item) => item.pr_number)
+        .filter(Boolean) as string[];
+
+      const existing = await prismaClient.purchaseRequest.findMany({
+        where: { pr_number: { in: prNumbers } },
+        select: { pr_number: true },
+      });
+
+      const existingNumbers = new Set(existing.map((e) => e.pr_number));
+
+      // Filter yang belum ada
+      const filteredRequest = createRequest.filter(
+        (item) => !existingNumbers.has(item.pr_number)
+      );
+      createRequestDetail = createRequestDetail.filter(
+        (item) => !existingNumbers.has(item.pr_number)
+      );
+
+      if (filteredRequest.length === 0) {
+        logger.error(
+          `All PR numbers in file ${filePath} already exist in database`
+        );
+      } else if (filteredRequest.length !== createRequest.length) {
+        logger.warn(
+          `Some PR numbers in file ${filePath} already exist in database`
+        );
+        existingNumbers.forEach((pr) =>
+          logger.warn(`PR Number ${pr} already exists in database`)
+        );
+      }
+
+      // Simpan ke database
+      await prismaClient.$transaction([
+        prismaClient.purchaseRequest.createMany({ data: filteredRequest }),
+        prismaClient.purchaseRequestDetail.createMany({
+          data: createRequestDetail,
+        }),
+      ]);
+
+      logger.info("Purchase request and details created successfully");
+      return true;
+    } catch (error) {
+      logger.error(
+        `Error while creating purchase request and details: ${error}`
+      );
+      throw new Error(
+        `Error while creating purchase request and details: ${error}`
+      );
+    }
+  }
+}
